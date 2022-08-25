1ed205bdcd1b089cc1d641355c02a1a703438f13
resat <ozkurtresat@gmail.com>
2022-08-25 22:26:02 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserProfileComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
