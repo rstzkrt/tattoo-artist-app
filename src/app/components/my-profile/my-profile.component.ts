f3ff7a726c69c903126ebdccc0475fb1c6d17c3b
resat <ozkurtresat@gmail.com>
2022-09-06 23:34:38 +0200

    my profile structure implemented

--- src/app/components/my-profile/my-profile.component.ts
+++ src/app/components/my-profile/my-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-my-profile',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public afAuth:AngularFireAuth) { }
 
   ngOnInit(): void {
   }
