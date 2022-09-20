b51c408980c124fc06872251ba160b9ea397764d
resat <ozkurtresat@gmail.com>
2022-09-20 17:56:00 +0200

    email/password - facebook login implemented

--- src/app/components/login-dialog/login-dialog.component.ts
+++ src/app/components/login-dialog/login-dialog.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../services/auth.service";
 import {MatDialog} from "@angular/material/dialog";
-import {FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-login-dialog',
@@ -9,15 +10,57 @@ import {FormGroup} from "@angular/forms";
   styleUrls: ['./login-dialog.component.css']
 })
 export class LoginDialogComponent implements OnInit {
-  emailSignUpFormGroup: FormGroup;
+  emailSignUpFormGroup: FormGroup
+  loginFormGroup: FormGroup;
 
-  constructor(public authService:AuthService,private dialog:MatDialog) { }
+  constructor(public authService:AuthService,
+              private userService:UserService,
+              private dialog:MatDialog,
+              private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.loginFormGroup = this.formBuilder.group({
+      emailLoginForm: this.formBuilder.group({
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+      })
+    })
 
+    this.emailSignUpFormGroup = this.formBuilder.group({
+      emailRegisterForm: this.formBuilder.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+      })
+    })
+  }
 
   submit() {
 
+      let user:EmailLoginReq = this.emailSignUpFormGroup.get('emailRegisterForm').value;
+      this.authService.singUpWithEmail(user)
+        .then()
+        .catch(err=>console.log(err))
+
   }
+
+  signIn() {
+    let login:Login = this.loginFormGroup.get('emailLoginForm').value;
+    this.authService.singInWithEmail(login)
+      .then()
+      .catch(err=>console.log(err))
+  }
+}
+
+export interface EmailLoginReq {
+  uid?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  password?:string
+}
+export interface Login {
+  email?: string;
+  password?:string
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,9 +8,11 @@ import {HttpClient} from "@angular/common/http";
 import {AngularFireFunctions} from "@angular/fire/compat/functions";
 import {StreamChat, TokenOrProvider} from "stream-chat";
 import {environment} from "../../environments/environment";
-import {LoginDialogComponent} from "../components/login-dialog/login-dialog.component";
+import {EmailLoginReq, Login, LoginDialogComponent} from "../components/login-dialog/login-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
-import {getAuth, deleteUser} from "firebase/auth";
+import {getAuth} from "firebase/auth";
+import {StorageService} from "./storage.service";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -28,26 +30,102 @@ export class AuthService {
               private userService: UserService,
               private httpClient: HttpClient,
               private firebaseFunctions: AngularFireFunctions,
-              public dialog: MatDialog) {
+              public dialog: MatDialog,
+              private storageService: StorageService,
+              private router: Router
+  ) {
 
     this.afAuth.authState.subscribe(user => {
       console.log(user)
       this.firebaseUser = user;
       if (user) {
         user.getIdToken().then((token) => {
+          this.storageService.saveToken(token)
           this.userObservable = userService.fetchAuthenticatedUser(token)
           userService.fetchAuthenticatedUser(token).subscribe(data => {
+            this.storageService.saveUser(data)
             this.authenticatedUser = data;
-            console.log(this.authenticatedUser)
           }, error => console.error(error))
+        })
+      }
+
+    })
+    this.afAuth.onIdTokenChanged(function (user) {
+      if (user) {
+        user.getIdToken().then(data => {
+          storageService.saveToken(data)
+          console.log("token update")
         })
       }
     });
   }
 
-  async loginWithGoogle() {
+  async singInWithEmail(user: Login) {
+    await this.afAuth.signInWithEmailAndPassword(user.email, user.password).then(
+      res => {
+        res.user.getIdToken().then(token => {
+          this.userService.fetchAuthenticatedUser(token).subscribe(user => {
+            let tokenFunction = this.firebaseFunctions.httpsCallable('ext-auth-chat-getStreamUserToken')
+            tokenFunction({}).subscribe(async data => {
+              const chatClient = StreamChat.getInstance(environment.stream.key);
+              await chatClient.connectUser({
+                  id: user.uid,
+                  name: user.firstName + " " + user.lastName,
+                  image: user.avatarUrl
+                },
+                data);
+              window.location.replace('/')
+            });
+          })
+        })
+        console.log("Login Success")
+        this.dialogRef.close()
+      }
+    ).catch(err => {
+      window.alert(err)
+      console.log(err)
+    })
+  }
+
+
+  async singUpWithEmail(user: EmailLoginReq) {
+
+      await this.afAuth.createUserWithEmailAndPassword(user.email, user.password).then(
+        async res => {
+          this.requestBodyUser.uid = res.user.uid
+          this.requestBodyUser.email = user.email;
+          this.requestBodyUser.firstName = user.firstName
+          this.requestBodyUser.lastName = user.lastName
+          this.requestBodyUser.avatarUrl = "https://www.gravatar.com/avatar/?d=mp"
+
+          this.userService.createClient(this.requestBodyUser).subscribe(user => {
+            let tokenFunction = this.firebaseFunctions.httpsCallable('ext-auth-chat-getStreamUserToken')
+            tokenFunction({}).subscribe(async data => {
+              const chatClient = StreamChat.getInstance(environment.stream.key);
+              await chatClient.connectUser({
+                  id: user.uid,
+                  name: user.firstName + " " + user.lastName,
+                  image: user.avatarUrl
+                },
+                data);
+              window.location.replace('/')
+            });
+            console.log(user);
+          }, error => {
+            console.log(error);
+          });
+          console.log("Login Success")
+          this.dialogRef.close()
+        }
+      ).catch(err => {
+        window.alert(err)
+        console.log(err)
+      })
+  }
+
+  async loginWithFacebook() {
     console.log('login')
-    await this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(
+    await this.afAuth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(
       async res => {
         this.requestBodyUser.uid = res.user.uid
         this.requestBodyUser.email = res.user.email;
@@ -60,9 +138,59 @@ export class AuthService {
           this.requestBodyUser.lastName = res.user.displayName.split(" ")[length];
         }
 
+        console.log(0)
         if (res.additionalUserInfo.isNewUser) {
-          this.userService.registerClient(this.requestBodyUser)
+          console.log(1)
+          this.userService.createClient(this.requestBodyUser)
             .subscribe(response => {
+              console.log(response);
+            }, error => {
+              console.log(error);
+            });
+        }
+        console.log(2)
+
+        let tokenFunction = this.firebaseFunctions.httpsCallable('ext-auth-chat-getStreamUserToken')
+        tokenFunction({}).subscribe(async data => {
+          const chatClient = StreamChat.getInstance(environment.stream.key);
+
+          await chatClient.connectUser({
+              id: this.requestBodyUser.uid,
+              name: this.requestBodyUser.firstName + " " + this.requestBodyUser.lastName,
+              image: this.requestBodyUser.avatarUrl
+            },
+            data);
+          console.log(4)
+          window.location.replace('/')
+          console.log(5)
+
+        });
+        console.log("Login Success")
+        this.dialogRef.close()
+        console.log(6)
+      }
+    ).catch(err => {
+      console.log(7)
+      console.log(err)
+    })
+    console.log(8)
+  }
+
+  async loginWithGoogle() {
+    await this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(
+      res => {
+        this.requestBodyUser.uid = res.user.uid
+        this.requestBodyUser.email = res.user.email;
+        this.requestBodyUser.avatarUrl = res.user.photoURL;
+        if (res.user.displayName.split(" ").length === 1) {
+          this.requestBodyUser.firstName = res.user.displayName;
+          this.requestBodyUser.lastName = "";
+        } else {
+          this.requestBodyUser.firstName = res.user.displayName.split(" ")[0];
+          this.requestBodyUser.lastName = res.user.displayName.split(" ")[length];
+        }
+        if (res.additionalUserInfo.isNewUser) {
+          this.userService.createClient(this.requestBodyUser).subscribe(response => {
               console.log(response);
             }, error => {
               console.log(error);
@@ -73,12 +201,13 @@ export class AuthService {
         tokenFunction({}).subscribe(async data => {
           const chatClient = StreamChat.getInstance(environment.stream.key);
           console.log(this.authenticatedUser)
-          await chatClient.connectUser(
-            {
-              id: this.authenticatedUser.uid,
-              name: this.authenticatedUser.firstName + " " + this.authenticatedUser.lastName,
-              image: this.authenticatedUser.avatarUrl,
-            }, data);
+          await chatClient.connectUser({
+              id: this.requestBodyUser.uid,
+              name: this.requestBodyUser.firstName + " " + this.requestBodyUser.lastName,
+              image: this.requestBodyUser.avatarUrl
+            },
+            data);
+          window.location.replace('/')
         });
         console.log("Login Success")
         this.dialogRef.close()
@@ -95,11 +224,12 @@ export class AuthService {
   logout() {
     this.afAuth.signOut().then(() => {
       console.log("Logged out!")
+      this.storageService.clean()
       this.firebaseFunctions.httpsCallable('ext-auth-chat-revokeStreamUserToken')
         .call(data => {
           console.log(data)
         });
-
+      window.location.replace('/home')
     });
     const chatClient = StreamChat.getInstance(environment.stream.key);
     chatClient.disconnectUser().then(r => {
@@ -107,13 +237,7 @@ export class AuthService {
   }
 
   deleteAccount() {
-    const auth = getAuth();
-    const user = auth.currentUser;
-    deleteUser(user).then(() => {
-      console.log("deleted")
-    }).catch((error) => {
-      console.log(error)
-    });
+    this.afAuth.currentUser.then(user => user?.delete())
   }
 
   getCurrentUser() {
